7bb39523f060df4d2bda0eaa5b9b14fc78b68b16
Aman1072 <97711315+Aman1072@users.noreply.github.com>
2022-07-23 04:44:59 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,6 @@
+function changeMe1(){
+    var h1 = document.getElementById('name').innerHTML = 'Aman Rabari';
+    var h3 = document.getElementById('status').innerHTML = 'UI Designer';
+    var p = document.getElementById('about').innerHTML = 'hi guys, I am Aman Rabari and I am UI designer in Quatro rebrand. We have been working together so we can make our website and our product more appealing to our customers.  My first approach to design the aesthetic experience of a product. Visual touch point that allow users to interact with a prduct.';
+    document.getElementById('profile').src = "./images/IMG-20190728-WA0004.jpg";
+}
